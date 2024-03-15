aec6c133ed5978a32ce3cd78654e76ca4b31fd12
Dayron-z <zbastian.zapata@gmail.com>
2024-03-14 20:14:23 -0500

    Listo

--- activity/src/EmpleadoPermanente.java
+++
@@ -1,6 +0,0 @@
-public class EmpleadoPermanente extends  Empleado{
-
-    public EmpleadoPermanente(String nombre, int edad, double salario) {
-        super(nombre, edad, salario);
-    }
-}
--- activity/src/EmpleadoTemporal.java
+++
@@ -1,9 +0,0 @@
-public class EmpleadoTemporal extends Empleado {
-/*    Heredan de Empleado y
-    representan diferentes tipos de empleados.*/
-
-
-    public EmpleadoTemporal(String nombre, int edad, double salario) {
-        super(nombre, edad, salario);
-    }
-}
